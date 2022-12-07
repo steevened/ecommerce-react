cf7b9c297fbfbeaf45ce46298883fb7a3356c162
steven <alvaradosteven315@gmail.com>
2022-12-07 12:09:59 -0500

    fixing bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,47 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import Filter from '../components/Filter'
-import SearchInput from '../components/SearchInput'
-import ShowFilter from '../components/ShowFilter'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Filter from '../components/Filter';
+import SearchInput from '../components/SearchInput';
+import ShowFilter from '../components/ShowFilter';
 import {
   filterHeadThunk,
   filterThunk,
   getProductsThunk,
-} from '../store/slices/products.slice'
-import { BsCart2 } from 'react-icons/bs'
-import Cart from '../components/Cart'
-import { postCartThunk } from '../store/slices/cart.slice'
+} from '../store/slices/products.slice';
+import { BsCart2 } from 'react-icons/bs';
+import Cart from '../components/Cart';
+import { postCartThunk } from '../store/slices/cart.slice';
 
 function Home({ cart, setCart, setModalShowed, modalShowed }) {
-  const [categories, setCategories] = useState([])
-  const [inputSearch, setInputSearch] = useState('')
-  const [minPrice, setMinPrice] = useState(null)
-  const [maxPrice, setMaxPrice] = useState(99999)
-  const [idImg, setIdImg] = useState(null)
-  const dispatch = useDispatch()
-  const products = useSelector((state) => state.products)
+  const [categories, setCategories] = useState([]);
+  const [inputSearch, setInputSearch] = useState('');
+  const [minPrice, setMinPrice] = useState(null);
+  const [maxPrice, setMaxPrice] = useState(99999);
+  const [idImg, setIdImg] = useState(null);
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
 
   useEffect(() => {
-    dispatch(getProductsThunk())
+    dispatch(getProductsThunk());
 
     axios
       .get('https://e-commerce-api.academlo.tech/api/v1/products/categories')
-      .then((res) => setCategories(res.data.data.categories))
-  }, [])
+      .then((res) => setCategories(res.data.data.categories));
+  }, []);
 
   const addtoCart = (id) => {
     const productBuyed = {
       id,
       quantity: 1,
-    }
+    };
     // console.log(productBuyed)
-    dispatch(postCartThunk(productBuyed))
-  }
+    dispatch(postCartThunk(productBuyed));
+  };
 
   return (
-    <div className='text-center relative overflow-hidden pt-16'>
+    <div className="text-center relative overflow-hidden pt-16">
       <Filter
         categories={categories}
         modalShowed={modalShowed}
@@ -54,10 +54,10 @@ function Home({ cart, setCart, setModalShowed, modalShowed }) {
       />
       <div
         onClick={() => {
-          setModalShowed(false)
-          setCart(false)
+          setModalShowed(false);
+          setCart(false);
         }}
-        className='py-10 md:ml-80'
+        className="py-10 md:ml-80"
       >
         <SearchInput
           inputSearch={inputSearch}
@@ -67,50 +67,50 @@ function Home({ cart, setCart, setModalShowed, modalShowed }) {
 
         <ShowFilter setModalShowed={setModalShowed} />
 
-        <ul className='text-left  grid xl:grid-cols-2  w-3/4 mx-auto my-10 gap-5'>
+        <ul className="text-left  grid xl:grid-cols-2  w-3/4 mx-auto my-10 gap-5">
           {products.map(
             (product) =>
               Number(product.price) > minPrice &&
               Number(product.price) < maxPrice && (
                 <div
-                  className='border-2 border-base-300  w-full '
+                  className="border-2 border-base-300  w-full "
                   key={product.id}
                 >
                   <Link
                     to={`/products/${product.id}`}
                     onMouseOver={() => {
-                      setIdImg(product.id)
+                      setIdImg(product.id);
                     }}
                     onMouseLeave={() => {
-                      setIdImg(null)
+                      setIdImg(null);
                     }}
-                    className='flex items-center justify-center w-full h-64 relative overflow-hidden '
+                    className="flex items-center justify-center w-full h-64 relative overflow-hidden "
                   >
                     <img
                       className={`absolute block object-contain w-5-6 h-5/6 transition-all duration-700 ${
                         idImg === product.id ? 'opacity-0' : ''
                       }`}
                       src={product.productImgs[0]}
-                      alt='product'
+                      alt="product"
                     />
                     <img
                       className={`absolute block object-contain  w-5-6 h-5/6  transition-all duration-700 ${
                         idImg !== product.id ? 'opacity-0' : ''
                       }`}
                       src={product.productImgs[1]}
-                      alt='product'
+                      alt="product"
                     />
                   </Link>
-                  <div className='border-t-2 border-base-300 relative h-32'>
-                    <div className='px-2 py-3 flex flex-col justify-between'>
-                      <h2 className='text-lg'>{product?.title}</h2>
-                      <p className='text-gray-500'>Price</p>
+                  <div className="border-t-2 border-base-300 relative h-32">
+                    <div className="px-2 py-3 flex flex-col justify-between">
+                      <h2 className="text-lg">{product?.title}</h2>
+                      <p className="text-gray-500">Price</p>
                       <p>{product?.price}</p>
                     </div>
                     <button
                       onClick={() => addtoCart(product.id)}
-                      className='btn btn-base text-xl  text-base-100 grid place-content-center btn-circle absolute bottom-3 right-3 tooltip tooltip-base tooltip-bottom'
-                      data-tip='Add to Cart'
+                      className="btn btn-base text-xl  text-base-100 grid place-content-center btn-circle absolute bottom-3 right-3 tooltip tooltip-base tooltip-bottom"
+                      data-tip="Add to Cart"
                     >
                       <BsCart2 />
                     </button>
@@ -121,7 +121,7 @@ function Home({ cart, setCart, setModalShowed, modalShowed }) {
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/store/slices/cart.slice.jsx
+++ src/store/slices/cart.slice.jsx
@@ -1,29 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
-import getConfig from '../../helpers/getConfig'
-import { setIsLoading } from './isLoading.slice'
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import getConfig from '../../helpers/getConfig';
+import { setIsLoading } from './isLoading.slice';
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: [],
   reducers: {
     getCart: (state, action) => {
-      return action.payload
+      return action.payload;
     },
   },
-})
+});
 
 export const getCartThunk = () => (dispatch) => {
-  dispatch(setIsLoading(true))
+  dispatch(setIsLoading(true));
   return axios
     .get('https://e-commerce-api.academlo.tech/api/v1/cart', getConfig())
     .then((res) => dispatch(getCart(res.data.data.cart.products)))
-    .finally(() => dispatch(setIsLoading(false)))
-    .catch((error) => console.log(error?.response.data))
-}
+    .finally(() => dispatch(setIsLoading(false)));
+  // .catch((error) => console.log(error?.response.data))
+};
 
 export const postCartThunk = (product) => (dispatch) => {
-  dispatch(setIsLoading(true))
+  dispatch(setIsLoading(true));
   return axios
     .post(
       'https://e-commerce-api.academlo.tech/api/v1/cart',
@@ -31,11 +31,11 @@ export const postCartThunk = (product) => (dispatch) => {
       getConfig()
     )
     .then(() => dispatch(getCartThunk()))
-    .finally(() => dispatch(setIsLoading(false)))
-}
+    .finally(() => dispatch(setIsLoading(false)));
+};
 
 export const checkoutThunk = () => (dispatch) => {
-  dispatch(setIsLoading(true))
+  dispatch(setIsLoading(true));
   return axios
     .post(
       'https://e-commerce-api.academlo.tech/api/v1/purchases',
@@ -43,20 +43,20 @@ export const checkoutThunk = () => (dispatch) => {
       getConfig()
     )
     .then(() => dispatch(getCart([])))
-    .finally(() => dispatch(setIsLoading(false)))
-}
+    .finally(() => dispatch(setIsLoading(false)));
+};
 
 export const deleteItemThunk = (id) => (dispatch) => {
-  dispatch(setIsLoading(true))
+  dispatch(setIsLoading(true));
   return axios
     .delete(
       `https://e-commerce-api.academlo.tech/api/v1/cart/${id}`,
       getConfig()
     )
     .then(() => dispatch(getCartThunk()))
-    .finally(() => dispatch(setIsLoading(false)))
-}
+    .finally(() => dispatch(setIsLoading(false)));
+};
 
-export const { getCart } = cartSlice.actions
+export const { getCart } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
